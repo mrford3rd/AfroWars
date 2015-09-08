4f05c904286b2bbed8bd61ed4a58ce9642615535
mrford3rd <mrford3rd@gmail.com>
2015-09-08 15:58:42 -0400

    Just added instructions for how Codeanywhere works with github

--- EndofGame.cs
+++ EndofGame.cs
@@ -1,3 +1,10 @@
 //This file is for EndofGame code
 
 //If player dies show final score and time survived
+
+//For Codeanywhere to work with Githup
+
+//1. You have to commit changes to files
+//2. Push changes for them to take effect
+
+//Remeber to do a pull request for fresh copy of files
