64303093245f332b896386309d9957abab787241
mrford3rd <mrford3rd@gmail.com>
2015-09-09 11:27:52 -0400

    This update adds Mini Map to the game using the Unity 5 UI

--- miniMap.cs
+++ miniMap.cs
@@ -1,2 +1,69 @@
-//This mini_map is based off the new Unity 5 UI
-/* Instructions: */
+//This mini_map is based off the new Unity 5 UI https://www.youtube.com/watch?v=EeyZ2y2Jpz4
+/*---------------------------------------------------------------------
+Use Instructions: 
+
+Player Object
+	Add blip script
+	Target = Player
+	
+Enemy Object
+	Add blip script
+	Target = Enemy
+	
+Create Panel
+	Anchor to Corner
+	Add two UI Images
+		1. Player Blip - set color and scale
+		2. Enemy Blip  - set color and scale
+	Add MiniMap Script
+		Target = player blip
+	Add UI Image - Level Texture
+		Set scale	
+
+Note: Requiers miniMapBlip script
+--------------------------------------------------------------------------*/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MiniMap : MonoBehavior
+{
+	public Transform Target;
+	public float ZoomLevel = 10f;
+	
+	Vector2 XRotation = Vector2.right;
+	Vector2 YRotation = Vector2.up;
+	
+	void LateUpdate()
+	{
+		XRotation = new Vector2(Target.right.x, -Target.right.z);	
+		YRotation = new Vector2(-Target.forward.x, Target.forward.z);
+	}
+	
+	public Vector2 TransformfromPosition(Vector3 position)
+	{
+		Vector3 offset = position - Target.position;
+		Vector2 newPosition = offset.x * XRotation;
+		newPosition += offset.z * YRotation;
+		
+		newPosition *= ZoomLevel;
+		
+		return newPosition;
+	}
+	
+	public Vecoter3 TransformRotation(Vector3 rotation)
+	{
+		return new Vecoter3(0,0, Target.eulerAngles.y - rotation.y)	;
+	}
+	
+	}
+	public Vector2 MoveInside(Vector2 point)
+	{
+		Rect mapRect = GetComponent<Rectransform>().rect;
+		point = Vector2.Max(point, mapRect.min);
+		point = Vector2.Min(point, mapRect.max);
+		
+		return point;
+	}
+	
+}
--- miniMapBlip.cs
+++ miniMapBlip.cs
@@ -1,1 +1,55 @@
-//This is placed on the blip for the mini map
+//This mini map blip is based off the new Unity 5 UI https://www.youtube.com/watch?v=EeyZ2y2Jpz4
+/*---------------------------------------------------------------------
+Use Instructions: 
+
+Player Object
+	Add blip script
+	Target = Player
+	
+Enemy Object
+	Add blip script
+	Target = Enemy	
+
+Note: Requiers miniMap script
+--------------------------------------------------------------------------*/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class miniMapBlip : MonoBehavior
+{
+	public Transform Target;
+	public bool KeepInBounds = true;
+	public bool LockScale = false;
+	public bool LockRotation = false;
+	public float MinScale = 1f;
+	MiniMap map;
+	RectTransform myRectTransform;
+	
+	void Start()
+	{
+		map = GetComponentInParrent<MiniMap>();
+		myRectTransform = GetComponent<RectTransform>();
+	}
+	
+	void LateUpdate()
+	{
+		Vector2 newPosition = map.TransformPosition(Target.position);
+		
+		if (KeepInBounds)
+			newPosition = map.MoveInside(newPosition);
+		
+		if(!LockScale)
+		{
+			float scale = Mathf.Max(MinScale, map.zoomLevel);
+			myRectTransform.localScale = new Vector3(scale, scale, 1);
+		}
+					
+		if(!LockRotation)
+			myRectTransform.localEulerAngels = map.TransformRotation(Target.EularAngels);
+		
+		myRectTransform.localPosition = newPosition;
+	}
+	
+	
+}
